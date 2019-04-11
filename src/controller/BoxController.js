de42eba1f8cba2414495e24d82c59200bcb124ae
Rodrigues Junior <31141974+GAOSHUU@users.noreply.github.com>
2019-04-11 18:49:55 -0300

    BackEnd completo

--- src/controller/BoxController.js
+++ src/controller/BoxController.js
@@ -8,6 +8,15 @@ class BoxController {
         
     }
 
+    async show(req, res) {
+        const box = await Box.findById(req.params.id).populate({
+            path: 'files',
+            options: { sort: { createdAt: -1 } }
+        });
+
+        return res.json(box);
+    }
+
 }
 
 module.exports = new BoxController();
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,17 @@
 const express = require('express');
-const BoxController = require('../controller/BoxController')
+const BoxController = require('./controller/BoxController')
+const multer = require('multer')
+const multerConfig = require('./config/multer')
 
 const view = express();
 
-view.set('views', './views');
+view.set('views', './src/views');
 view.set('view engine', 'pug');
 view.get('/index', (req, res) => {
     res.render('index', ({title : 'Tobias'}));
 });
 view.post('/boxes', BoxController.store)
+view.get('/boxes/id:', BoxController.show)
+view.post('/boxes/:id/files', multer(multerConfig).single(), BoxController.store)
 
 module.exports = view;
