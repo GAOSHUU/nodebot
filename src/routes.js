3b8836d9e8e0dac7a35d5681f2b914ae62198594
Rodrigues Junior <31141974+GAOSHUU@users.noreply.github.com>
2019-04-11 19:54:30 -0300

    exclusão do view

--- src/routes.js
+++ src/routes.js
@@ -5,11 +5,6 @@ const multerConfig = require('./config/multer')
 
 const view = express();
 
-view.set('views', './src/views');
-view.set('view engine', 'pug');
-view.get('/index', (req, res) => {
-    res.render('index', ({title : 'Tobias'}));
-});
 view.post('/boxes', BoxController.store)
 view.get('/boxes/id:', BoxController.show)
 view.post('/boxes/:id/files', multer(multerConfig).single(), BoxController.store)
